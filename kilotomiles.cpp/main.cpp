f2a191b522df4df11067d00b2cc5cc0201605aa3
mohamedKamel2020 <m.kamelmohamed2020@gmail.com>
2023-10-12 21:36:37 +0300

    add program kiloToMiles for Assignment 2

---
+++ kilotomiles.cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float kilometersTraveled;
+    cout <<"Please, Enter  the number of kilometers traveled :";
+    cin >>kilometersTraveled;
+    cout <<"The number of miles traveled : "<<kilometersTraveled*0.621<<" miles";
+    return 0;
+}
