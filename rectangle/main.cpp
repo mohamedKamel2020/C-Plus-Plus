49fec51afa46cdef4ded9fbb673551d2cda318a2
mohamedKamel2020 <m.kamelmohamed2020@gmail.com>
2023-10-12 21:43:56 +0300

    add program rectangle for Assignment 2

---
+++ rectangle/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+const int length=8;
+const int width=3;
+int main()
+{
+    cout << "area is : "<<length*width<<endl;
+    cout << "perimeter is : "<<2*(length+width)<<endl;
+
+    return 0;
+}
